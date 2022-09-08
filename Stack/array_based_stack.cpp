c521ed125b72ed04e1823168523aa113aa2e619e
MostafaAE <mostafa.aymanae@gmail.com>
2022-09-08 15:54:46 +0200

    insert at bottom

--- Stack/array_based_stack.cpp
+++ Stack/array_based_stack.cpp
@@ -52,22 +52,48 @@ public:
             cout << array[i] << " ";
         cout << "\n";
     }
+
+    void insert_at_bottom_iterative(int value)
+    {
+        assert(!isFull());
+        top++;
+        for (int i = top; i > 0; i--)
+            array[i] = array[i - 1];
+        array[0] = value;
+    }
+
+    void insert_at_bottom(int value)
+    {
+        if (isEmpty())
+        {
+            push(value);
+            return;
+        }
+
+        int cur = pop();
+        insert_at_bottom(value);
+        push(cur);
+    }
 };
 
 int main()
 {
-    Stack stack(3);
+    Stack stack(5);
     stack.push(10);
     stack.push(20);
     stack.push(30);
 
-    cout << stack.isFull() << endl;
-    cout << stack.peek() << endl;
-    stack.display();
-    stack.pop();
+    // cout << stack.isFull() << endl;
+    // cout << stack.peek() << endl;
+    // stack.display();
+    // stack.pop();
 
-    cout << stack.isFull() << endl;
-    cout << stack.peek() << endl;
+    // cout << stack.isFull() << endl;
+    // cout << stack.peek() << endl;
+    stack.display();
+
+    stack.insert_at_bottom_iterative(40);
+    stack.insert_at_bottom(50);
     stack.display();
 
     // must see it, otherwise RTE
