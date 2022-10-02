9e87eb85e1c98d65c245dc808936e843a852d8a1
MostafaAE <mostafa.aymanae@gmail.com>
2022-10-02 15:23:05 +0200

    AVL Tree insertion

--- AVL Tree/avl_tree.cpp
+++ AVL Tree/avl_tree.cpp
@@ -29,7 +29,7 @@ struct Node
     }
 };
 
-class BinarySearchTree
+class AVLTree
 {
 
 private:
@@ -102,15 +102,82 @@ private:
         return node;
     }
 
-public:
-    BinarySearchTree() {}
+    Node *insert(int target, Node *node)
+    {
+        if (target < node->data)
+        {
+            if (node->left)
+                node->left = insert(target, node->left);
+            else
+                node->left = new Node(target);
+        }
+        else if (target > node->data)
+        {
+            if (node->right)
+                node->right = insert(target, node->right);
+            else
+                node->right = new Node(target);
+        }
 
-    BinarySearchTree(int root_value)
+        node->update_height();
+        return balance(node);
+    }
+
+    void print_inorder(Node *node)
+    {
+        if (!node)
+            return;
+
+        print_inorder(node->left);
+        cout << node->data << " ";
+        print_inorder(node->right);
+    }
+
+    bool is_bst(Node *node, int mn = INT_MIN, int mx = INT_MAX)
+    {
+        bool status = mn < node->data && node->data < mx;
+
+        if (!status)
+            return false;
+
+        bool left_bst{1}, right_bst{1};
+
+        if (node->left)
+        {
+            if (node->data > node->left->data)
+                left_bst = is_bst(node->left, mn, node->data);
+            else
+                left_bst = false;
+        }
+
+        if (left_bst && node->right)
+        {
+            if (node->data < node->right->data)
+                right_bst = is_bst(node->right, node->data, mx);
+            else
+                right_bst = false;
+        }
+
+        return left_bst && right_bst;
+    }
+
+    void verify()
+    {
+        assert(abs(root->balance_factor()) <= 1);
+        assert(is_bst(root));
+    }
+
+public:
+    AVLTree()
+    {
+    }
+
+    AVLTree(int root_value)
     {
         root = new Node(root_value);
     }
 
-    ~BinarySearchTree()
+    ~AVLTree()
     {
         clear();
     }
@@ -126,12 +193,244 @@ public:
         assert(root);
         return search(target, root);
     }
+
+    void insert(int target)
+    {
+        if (!root)
+            root = new Node(target);
+
+        root = insert(target, root);
+    }
+
+    void print_inorder()
+    {
+        print_inorder(root);
+    }
+
+    void level_order_traversal()
+    {
+        if (!root)
+            return;
+
+        cout << "******************\n";
+        queue<Node *> nodes_queue;
+        nodes_queue.push(root);
+
+        int level = 0;
+        while (!nodes_queue.empty())
+        {
+            int sz = nodes_queue.size();
+
+            cout << "Level " << level << ": ";
+            while (sz--)
+            {
+                Node *cur = nodes_queue.front();
+                nodes_queue.pop();
+
+                cout << cur->data << " ";
+                if (cur->left)
+                    nodes_queue.push(cur->left);
+                if (cur->right)
+                    nodes_queue.push(cur->right);
+            }
+            level++;
+            cout << "\n";
+        }
+    }
 };
 
+void f1()
+{
+    AVLTree tree;
+
+    for (int i = 0; i <= 32; ++i)
+    { // degenerate
+        tree.insert(i);
+        tree.level_order_traversal();
+    }
+}
 int main()
 {
-
+    f1();
     // must see it, otherwise RTE
     cout << "\n\nNO RTE\n";
     return 0;
 }
+
+/*
+
+
+ Level 0: 0
+ Level 1: 1
+ ******************
+ Level 0: 1
+ Level 1: 0 2
+ ******************
+ Level 0: 1
+ Level 1: 0 2
+ Level 2: 3
+ ******************
+ Level 0: 1
+ Level 1: 0 3
+ Level 2: 2 4
+ ******************
+ Level 0: 3
+ Level 1: 1 4
+ Level 2: 0 2 5
+ ******************
+ Level 0: 3
+ Level 1: 1 5
+ Level 2: 0 2 4 6
+ ******************
+ Level 0: 3
+ Level 1: 1 5
+ Level 2: 0 2 4 6
+ Level 3: 7
+ ******************
+ Level 0: 3
+ Level 1: 1 5
+ Level 2: 0 2 4 7
+ Level 3: 6 8
+ ******************
+ Level 0: 3
+ Level 1: 1 7
+ Level 2: 0 2 5 8
+ Level 3: 4 6 9
+ ******************
+ Level 0: 3
+ Level 1: 1 7
+ Level 2: 0 2 5 9
+ Level 3: 4 6 8 10
+ ******************
+ Level 0: 7
+ Level 1: 3 9
+ Level 2: 1 5 8 10
+ Level 3: 0 2 4 6 11
+ ******************
+ Level 0: 7
+ Level 1: 3 9
+ Level 2: 1 5 8 11
+ Level 3: 0 2 4 6 10 12
+ ******************
+ Level 0: 7
+ Level 1: 3 11
+ Level 2: 1 5 9 12
+ Level 3: 0 2 4 6 8 10 13
+ ******************
+ Level 0: 7
+ Level 1: 3 11
+ Level 2: 1 5 9 13
+ Level 3: 0 2 4 6 8 10 12 14
+ ******************
+ Level 0: 7
+ Level 1: 3 11
+ Level 2: 1 5 9 13
+ Level 3: 0 2 4 6 8 10 12 14
+ Level 4: 15
+ ******************
+ Level 0: 7
+ Level 1: 3 11
+ Level 2: 1 5 9 13
+ Level 3: 0 2 4 6 8 10 12 15
+ Level 4: 14 16
+ ******************
+ Level 0: 7
+ Level 1: 3 11
+ Level 2: 1 5 9 15
+ Level 3: 0 2 4 6 8 10 13 16
+ Level 4: 12 14 17
+ ******************
+ Level 0: 7
+ Level 1: 3 11
+ Level 2: 1 5 9 15
+ Level 3: 0 2 4 6 8 10 13 17
+ Level 4: 12 14 16 18
+ ******************
+ Level 0: 7
+ Level 1: 3 15
+ Level 2: 1 5 11 17
+ Level 3: 0 2 4 6 9 13 16 18
+ Level 4: 8 10 12 14 19
+ ******************
+ Level 0: 7
+ Level 1: 3 15
+ Level 2: 1 5 11 17
+ Level 3: 0 2 4 6 9 13 16 19
+ Level 4: 8 10 12 14 18 20
+ ******************
+ Level 0: 7
+ Level 1: 3 15
+ Level 2: 1 5 11 19
+ Level 3: 0 2 4 6 9 13 17 20
+ Level 4: 8 10 12 14 16 18 21
+ ******************
+ Level 0: 7
+ Level 1: 3 15
+ Level 2: 1 5 11 19
+ Level 3: 0 2 4 6 9 13 17 21
+ Level 4: 8 10 12 14 16 18 20 22
+ ******************
+ Level 0: 15
+ Level 1: 7 19
+ Level 2: 3 11 17 21
+ Level 3: 1 5 9 13 16 18 20 22
+ Level 4: 0 2 4 6 8 10 12 14 23
+ ******************
+ Level 0: 15
+ Level 1: 7 19
+ Level 2: 3 11 17 21
+ Level 3: 1 5 9 13 16 18 20 23
+ Level 4: 0 2 4 6 8 10 12 14 22 24
+ ******************
+ Level 0: 15
+ Level 1: 7 19
+ Level 2: 3 11 17 23
+ Level 3: 1 5 9 13 16 18 21 24
+ Level 4: 0 2 4 6 8 10 12 14 20 22 25
+ ******************
+ Level 0: 15
+ Level 1: 7 19
+ Level 2: 3 11 17 23
+ Level 3: 1 5 9 13 16 18 21 25
+ Level 4: 0 2 4 6 8 10 12 14 20 22 24 26
+ ******************
+ Level 0: 15
+ Level 1: 7 23
+ Level 2: 3 11 19 25
+ Level 3: 1 5 9 13 17 21 24 26
+ Level 4: 0 2 4 6 8 10 12 14 16 18 20 22 27
+ ******************
+ Level 0: 15
+ Level 1: 7 23
+ Level 2: 3 11 19 25
+ Level 3: 1 5 9 13 17 21 24 27
+ Level 4: 0 2 4 6 8 10 12 14 16 18 20 22 26 28
+ ******************
+ Level 0: 15
+ Level 1: 7 23
+ Level 2: 3 11 19 27
+ Level 3: 1 5 9 13 17 21 25 28
+ Level 4: 0 2 4 6 8 10 12 14 16 18 20 22 24 26 29
+ ******************
+ Level 0: 15
+ Level 1: 7 23
+ Level 2: 3 11 19 27
+ Level 3: 1 5 9 13 17 21 25 29
+ Level 4: 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30
+ ******************
+ Level 0: 15
+ Level 1: 7 23
+ Level 2: 3 11 19 27
+ Level 3: 1 5 9 13 17 21 25 29
+ Level 4: 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30
+ Level 5: 31
+ ******************
+ Level 0: 15
+ Level 1: 7 23
+ Level 2: 3 11 19 27
+ Level 3: 1 5 9 13 17 21 25 29
+ Level 4: 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 31
+ Level 5: 30 32
+
+
+ */
