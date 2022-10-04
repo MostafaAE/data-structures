ff2da5e47957da7aeda1948608f17fbc5b812090
MostafaAE <mostafa.aymanae@gmail.com>
2022-10-04 14:46:09 +0200

    trie insertion

--- Letter Tree (Trie)/trie.cpp
+++ Letter Tree (Trie)/trie.cpp
@@ -1,24 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class trie
+class Trie
 {
 private:
     static const int MAX_CHAR = 26;
-    trie *child[MAX_CHAR];
+    Trie *child[MAX_CHAR];
     bool isLeaf{};
 
 public:
-    trie()
+    Trie()
     {
         // set the child array pointers to nullptr
         memset(child, 0, size(child));
+    }
+
+    void insert(string str, int idx = 0)
+    {
+
+        if (idx == (int)str.size())
+            this->isLeaf = true;
+
+        else
+        {
+            // get the index of the letter
+            int cur = str[idx] - 'a';
+            if (!child[cur])
+                child[cur] = new Trie();
+
+            child[cur]->insert(str, idx + 1);
+        }
     }
 };
 
 int main()
 {
-
+    Trie tr;
+    tr.insert("abcdef");
+    tr.insert("bcdef");
     // must see it, otherwise RTE
     cout << "\n\nNO RTE\n";
     return 0;
