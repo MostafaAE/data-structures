92793405ded773ac1e8fe57f1c3877d8267355f8
MostafaAE <mostafa.aymanae@gmail.com>
2022-10-05 20:37:21 +0200

    get all strings in trie

--- Letter Tree (Trie)/trie.cpp
+++ Letter Tree (Trie)/trie.cpp
@@ -111,6 +111,19 @@ public:
         }
         return str;
     }
+
+    // O(NL) where N is the number of words and L is the word length
+    void get_all_strings(vector<string> &res, string cur_str = "")
+    {
+        if (this->isLeaf)
+            res.push_back(cur_str);
+
+        for (int i = 0; i < MAX_CHAR; i++)
+        {
+            if (child[i])
+                child[i]->get_all_strings(res, cur_str + (char)('a' + i));
+        }
+    }
 };
 
 void test1()
@@ -161,12 +174,30 @@ void test3()
     cout << tr.first_word_prefix("azz") << endl;    // a
 }
 
+void test4()
+{
+    Trie tr;
+    tr.insert_iterative("abcd");
+    tr.insert_iterative("xyz");
+    tr.insert_iterative("a");
+    tr.insert_iterative("ab");
+    tr.insert_iterative("xyzw");
+    tr.insert_iterative("bcd");
+
+    vector<string> res;
+    tr.get_all_strings(res);
+
+    for (string s : res)
+        cout << s << endl;
+}
+
 int main()
 {
 
     // test1();
     // test2();
-    test3();
+    // test3();
+    test4();
 
     // must see it, otherwise RTE
     cout << "\n\nNO RTE\n";
