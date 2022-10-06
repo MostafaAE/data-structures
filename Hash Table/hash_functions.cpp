4a6a669a523914d031b9964b2a3e29fd7ed13df0
MostafaAE <mostafa.aymanae@gmail.com>
2022-10-06 15:07:41 +0200

    hash string folding

--- Hash Table/hash_functions.cpp
+++ Hash Table/hash_functions.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int hash_string(string str, int n)
+int hash_string(string str, int n_)
 {
-    long long nn = n;
+    long long n = n_;
     long long sum = 0;
     for (int i = 0; i < (int)str.size(); i++)
-        sum = (sum * 26 + str[i] - 'a') % nn;
-    return sum % nn;
+        sum = (sum * 26 + str[i] - 'a') % n;
+    return sum % n;
 }
 
-int hash_string_lower_upper_digits(string str, int n)
+int hash_string_lower_upper_digits(string str, int n_ = 65407)
 {
-    long long nn = n;
+    long long n = n_;
     long long sum = 0;
     int base = 2 * 26 + 10; // lower + upper + digits
 
@@ -20,7 +20,7 @@ int hash_string_lower_upper_digits(string str, int n)
     {
         // lower [0 - 25], upper [26 - 51], digits [52 - 61]
         int value = 0;
-        if (islower[str[i]])
+        if (islower(str[i]))
             value = str[i] - 'a';
         else if (isupper(str[i]))
             value = 26 + str[i] - 'A';
@@ -29,14 +29,28 @@ int hash_string_lower_upper_digits(string str, int n)
         else
             assert(false);
 
-        sum = (sum * base + value) % nn;
+        sum = (sum * base + value) % n;
     }
 
-    return sum % nn;
+    return sum;
+}
+
+int hash_string_folding(string str, int n_ = 65407)
+{
+    long long n = n_;
+    long long sum = 0;
+    for (int i = 0; i < (int)str.size(); i += 4)
+    {
+        string block = str.substr(i, 4);
+        sum += hash_string_lower_upper_digits(block);
+        sum %= n;
+    }
+    return sum;
 }
 
 int main()
 {
+    cout << hash_string_folding("aabcdefgAxT334gfg"); // 635
 
     // must see it, otherwise RTE
     cout << "\n\nNO RTE\n";
